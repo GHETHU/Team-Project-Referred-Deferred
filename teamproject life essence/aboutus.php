8a28d51b2afe1505f54b02c53903a8f76331cecf
GHETHU <148248839+GHETHU@users.noreply.github.com>
2024-08-03 14:23:06 +0100

    Update aboutus.php

--- teamproject life essence/aboutus.php
+++ teamproject life essence/aboutus.php
@@ -1,10 +1,47 @@
 <?php
 ?>
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>Life Essence - About Us</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+
+    <link rel="stylesheet" href="style.css">
+    <title>About Us</title>
 </head>
 <body>
+    </body>
+<div class="about">
+        <div class="container">
+            <div class="content-section">
+                <div class="title" >
+            <h1>About Us</h1>
+            <p class="text">
+                At Life Essence, we believe in the power of nature to heal and rejuvenate. Our mission is to provide high-quality, all-natural herbal medicines that promote wellness and balance in everyday life. 
+                Rooted in ancient traditions and supported by modern science, our carefully crafted products are designed to enhance your health and vitality. 
+                We are committed to sustainability, ethical sourcing, and delivering excellence in every bottle. Discover the essence of life with our range of herbal remedies, and embark on a journey to a healthier, more harmonious you.
+
+            </p>
+            
+            </div>
+    </div>
+
+              <div class="image-section">
+        <img src="herbal-medicines.jpg" alt="Herbal Medicines" height="400" width="400">
+    </div>
+        
+            
+        
+
+        </div>
+        <div class="social">
+            <a href=""><i class= "fab fa-facebook-f"></i></a>
+            <a href=""><i class= "fab fa-instagram"></i></a>
+            <a href=""><i class= "fab fa-twitter"></i></a>
+        
+    </div>
+    </div>
 </body>
 </html>
