7fe0724b6487fdcd78706968fc1e473db22221b8
GHETHU <148248839+GHETHU@users.noreply.github.com>
2024-08-04 13:29:55 +0100

    Update aboutus.php

--- teamproject life essence/aboutus.php
+++ teamproject life essence/aboutus.php
@@ -45,3 +45,87 @@
     </div>
 </body>
 </html>
+
+
+
+
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+* {
+margin:0px;
+padding:0px;
+box-sizing: border-box;
+font-family: 'Poppins' , sans-serif;
+}
+
+.section{
+    width: 100%;
+    min-height: 100vh;
+    background-color: #ddd;
+}
+
+.container{
+    width:80%;
+    display: block;
+    margin:auto;
+    padding-top: 100px;
+}
+
+.content section{
+    float: left;
+width: 55%;
+}
+
+.content section .content h3{
+    margin-top: 20px;
+    color: #5d5d5d;
+}
+
+.content-section .content p{
+    margin-top: 10px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 18px;
+    line-height: 1.5;
+}
+
+.content-section .social {
+    margin-top: 40px;
+}
+
+.content-section .social i {
+    color: #a52a2a;
+    font-size: 50px;
+    padding: 0px 10px;
+}
+
+.content-section .social i{
+    color: #3d3d3d;
+}
+@media screen and (max-width: 768px) {
+    .container {
+        width: 80%;
+        display: block;
+        margin: auto;
+        padding-top: 50px;
+    }
+}
+
+    .content-section{
+        float: none;
+        width: 100%;
+        display: block;
+        margin: auto;
+    }
+    .image-section {
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    
+    .image-section img {
+        max-width: 100%;
+        height: auto;
+        border-radius: 8px;
+    }
+
+   
