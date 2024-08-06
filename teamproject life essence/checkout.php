f51f9ba99174ba28b852bc6b784665d41af994ef
ayham118 <147047594+ayham118@users.noreply.github.com>
2024-08-06 18:28:31 +0400

    Update checkout.php
    changed path for image

--- teamproject life essence/checkout.php
+++ teamproject life essence/checkout.php
@@ -58,7 +58,7 @@
 
                 <div class="input-field">
                     <label>Accepted Cards :</label>
-                    <img src="Visa-Mastercard-1-1024x378.webp" alt="Card Images">
+                    <img src="teamproject life essence/img/Visa-Mastercard-1-1024x378.webp" alt="Card Images">
                 </div>
                 <div class="input-field">
                     <label>Name on Card :</label>
