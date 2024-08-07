4e76c2c495c2cef3811b40c32a7c8ca8262a9967
SalarZB <148250566+SalarZB@users.noreply.github.com>
2024-08-07 23:47:08 +0100

    Update basket.php
    Code is added

--- teamproject life essence/basket.php
+++ teamproject life essence/basket.php
@@ -1,4 +1,7 @@
 <?php
+require_once('connectdatabase.php');
+$query = "SELECT * FROM `basket`;";
+$result = $db->query($query);
 ?>
 
 <html>
@@ -6,5 +9,25 @@
     <title>Life Essence - Basket</title>
 </head>
 <body>
+    <div id='maindiv' style='color:#62C0BF';>
+   <?php  
+if ($result->num_rows > 0)  
+    { 
+        // OUTPUT DATA OF EACH ROW 
+        while($row = $result->fetch_assoc()) 
+        { 
+            echo "Basket id " . 
+                $row["b_id"]. " - User ID: " . 
+                $row["u_id"]. " | Product: " .  
+                $row["product_ids"]. " | Processed " .  
+                $row["processed"]. "<br>"; 
+        } 
+    }  
+    else { 
+        echo "Basket is empty"; 
+    } 
+?>
+        
+    </div>
 </body>
 </html>
