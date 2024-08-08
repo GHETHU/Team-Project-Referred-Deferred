e34935b35fa0c0cf3275bc782dfed4db60f6d1ed
Fynnley-Aston <164901546+Fynnley-Aston@users.noreply.github.com>
2024-08-08 20:01:49 +0100

    Update index.php
    missing a single <li>

--- teamproject life essence/index.php
+++ teamproject life essence/index.php
@@ -34,7 +34,7 @@ $username = isset($_SESSION['username']) ? $_SESSION['username'] : 'Guest';
         </div>
     </div>
     <div class="dropdown_menu">
-        <a href='listing.php'>Products</a>
+        <li><a href='listing.php'>Products</a></li>
             <li><a href="basket.php">Basket</a></li>
             <li><a href='login.php'>Login</a></li>
             <li><a href='signup.php'>Signup</a></li>
