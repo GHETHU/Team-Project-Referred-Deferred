673e1e187e58f86fa8adb42e9c8766885d92077e
Fynnley-Aston <164901546+Fynnley-Aston@users.noreply.github.com>
2024-08-08 13:41:45 +0100

    Update contactus.php

--- teamproject life essence/contactus.php
+++ teamproject life essence/contactus.php
@@ -1,17 +1,7 @@
 <?php
-//connecting time !
-$db_host = 'localhost';
-$db_name = 'u_220192145_aproject';
-$username = 'u-220192145';
-$password = '020Z2XvJGO02ikG';
-
-try {
-    $db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password);
-} catch (PDOException $ex) {
-    echo("Failed to connect to database;<br>");
-    echo($ex->getMessage());
-    exit;
-}
+session_start();
+include 'navbar.php';
+require_once('connectdatabase.php');
 
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
