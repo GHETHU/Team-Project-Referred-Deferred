1aa9047a5483ccd0fc83b6c71fa2633611eca433
Fynnley-Aston <164901546+Fynnley-Aston@users.noreply.github.com>
2024-08-09 12:43:24 +0100

    Update ThankYou.php file links

--- teamproject life essence/ThankYou.php
+++ teamproject life essence/ThankYou.php
@@ -8,16 +8,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Thank You</title>
-    <link rel="stylesheet" href="teamproject life essence/css/ThankYou.css">
+    <link rel="stylesheet" href="css/ThankYou.css">
 </head>
 <body>
 
 <div class="wrapper">
 
     <div class="content">
-        <img src="logo.png" alt="Company Logo">
+        <img src="img/logo.png" alt="Company Logo">
         <h1>Thank You for Choosing Life Essence</h1>
-        <a href="teamproject life essence/index.php" class="home-button">Go to Homepage</a>
+        <a href="index.php" class="home-button">Go to Homepage</a>
     </div>
 
 </div>    
